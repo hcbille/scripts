2c9d47332aba300ac48f901d60a8d57dd19bbcbb
hc <hcbille@gmail.com>
2023-04-02 13:00:24 +0200

    edited splitbooks

--- audiobooks/splitbooks.sh
+++ audiobooks/splitbooks.sh
@@ -2,7 +2,7 @@
 for file in *.mp3; do
 	filename="${file%.*}"
 	mkdir -p "/audiobook/$filename"
-	ffmpeg -i "$file" -f segment -segment_time 3600 -c copy "/audiobook/$filename/$filename_%03d.mp3"
+	ffmpeg -i "$file" -f segment -segment_time 3600 -c copy "/audiobook/$filename/$filename-%03d.mp3"
 	mkdir -p archive/
 	mv "$file" archive/
 done
