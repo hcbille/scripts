edbfdc4f7076aec38eac7351779c234e1eb23c42
hcbille <32437363+hcbille@users.noreply.github.com>
2023-06-10 23:52:04 +0200

    updated install script

--- download/install.sh
+++ download/install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Check if Python is installed
+if ! command -v python &> /dev/null; then
+    echo "Python not found. Installing Python..."
+    sudo apt-get update
+    sudo apt-get install -y python
+fi
+
+# Check if pip is installed
+if ! command -v pip &> /dev/null; then
+    echo "pip not found. Installing pip..."
+    sudo apt-get update
+    sudo apt-get install -y python-pip
+fi
+
+# Check if ffmpeg is installed
+if ! command -v ffmpeg &> /dev/null; then
+    echo "ffmpeg not found. Installing ffmpeg..."
+    sudo apt-get update
+    sudo apt-get install -y ffmpeg
+fi
+
 # Install PyYAML
 pip install pyyaml
+
+# Install audiobook-dl
 pip install audiobook-dl
